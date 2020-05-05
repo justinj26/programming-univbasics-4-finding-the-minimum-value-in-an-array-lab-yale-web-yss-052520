b28eaebdf28cccbead447a17080352ea47b75f18
Justin James <justin.james@yale.edu>
2020-05-05 08:24:20 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,4 +1,11 @@
 def find_min_value(array)
  count = 0 
- min_value = 
+ min_value = 1 / 0.0 
+ while count < array.length do
+   if array[count] < min_value 
+     min_value = array[count]
+   end
+   count += 1 
+ end
+ min_value 
 end
